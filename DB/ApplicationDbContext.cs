658b76fe7d412e4a304ee154793a2717651a3898
sajee2003 <kanesarajasajeevan295@gmail.com>
2025-04-23 21:08:44 +0530

    first commit

---
+++ DB/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace EventBookingManagementSystem_Backend.DB
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
